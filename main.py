b3d41c6fb4b07d00a9dd765f50dc8bea8731e27a
rafal <rafal.fota@poczta.onet.pl>
2024-11-25 21:35:41 +0100

    gitignore pycache

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     workspace = cwd + '/jezdnie_torun'
     shp_points = 'pkty.shp'
     shp_path = 'jezdnie.shp'
-    shp_result = "result_points1"
-    shp_result_alt = "result_points_alt1"
+    shp_result = "result_points3"
+    shp_result_alt = "result_points_alt3"
     project_path = workspace + r'\MyProject3.aprx'
 
     load_shp_into_graph(workspace, shp_path, graph)
@@ -17,14 +17,14 @@ if __name__ == "__main__":
     a = graph.nodes[start]
     b = graph.nodes[end]
 
-    result, used_edges = graph.astar(a, b) #zmiana metody w zależności od najszybsza trasa/najkrótsza
+    result, used_edges = graph.astar_fastest(a, b) #zmiana metody w zależności od trasy najszybsza/najkrotsza
 
     # Znalezienie alternatywnej trasy kiedy te z pierwszej mają podwojoną długość
     for edge in used_edges:
-        edge.length = edge.length * 2
+        edge.time_cost = edge.time_cost * 2  #dla najkrotszej: .length, dla najszybszej: .time_cost
 
     graph.reset_nodes()
-    result_alt, used_edges_alt = graph.astar(a, b) #zmiana metody w zależności od najszybsza trasa/najkrótsza
+    result_alt, used_edges_alt = graph.astar_fastest(a, b) #zmiana metody w zależności od trasy najszybsza/najkrotsza
 
     # Zapis warstwy do shp
     save_shp(workspace, shp_path, shp_result, project_path, used_edges)
